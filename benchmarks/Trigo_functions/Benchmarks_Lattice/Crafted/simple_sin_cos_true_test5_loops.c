c925b51a6250ef51bdfba12c7c8df11d483417d4
Karine Even <karine.even_mendoza@kcl.ac.uk>
2019-04-08 23:18:30 +0100

    update benchmark

--- benchmarks/Trigo_functions/Benchmarks_Lattice/Crafted/simple_sin_cos_true_test5_loops.c
+++ benchmarks/Trigo_functions/Benchmarks_Lattice/Crafted/simple_sin_cos_true_test5_loops.c
@@ -6,27 +6,31 @@ int main()
 	double theta = 3.14;
 	double phi = 3.14;
 	double sphere_x = 0.0;
-   			
-	double x_y = phi + theta;
-	double temp = sin(x_y);
-
+        double temp = 0;
+   
 	// unwind 10 need
         for (int i = 0; i < M_PI; i++)
 	{
 	        for (int j = 0; j < M_PI; j++)
 		{	
 			double sin_x = sin(theta);
+			double cos_x = cos(theta);
+			double cos_y = cos(phi);
 			double sin_y = sin(phi);
-			double cos_x = xcos(theta);
-			double cos_y = xcos(phi);
-			double sum = (sin_x*cos_y + sin_y*cos_x);
+			double sum = (sin_x*cos_y + cos_x*sin_y);
 
 			sphere_x += sum;
+                       
+                        double x_y = phi + theta;		
+			temp += sin(x_y);
 		}
 	}
 
+	//double x_y = phi + theta;
+	//double temp = sin(x_y);
 
-        // ALL UNSAT; 
+        // ALL UNSAT;
+	assert(sphere_x == temp); // UNSAT 
 	assert(sphere_x < 0); //UNSAT
 	assert(sphere_x > 0); //SAT 
 }
