c7645d7ac2943d8dd8f8d87de2d60e15f935f49d
Karine Even <karine.even_mendoza@kcl.ac.uk>
2019-05-17 12:25:02 +0100

    final validation tests results

--- benchmarks/Modulo_function/Full_Version/validation_tests/tests/z_div_nz_opp_full_2_false.c
+++ benchmarks/Modulo_function/Full_Version/validation_tests/tests/z_div_nz_opp_full_2_false.c
@@ -10,7 +10,7 @@ int main()
     __CPROVER_assume(b != 0);
     __CPROVER_assume(m == 0);
 
-    int m1 = ((-a)/b)%,b;
+    int m1 = ((-a)/b)%b;
     int m2 = (-(a/b)-1)%b;
     assert(m1 == m2);		
 }
